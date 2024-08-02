ed101cff9c319a050eec97b6af2a0336bdae1816
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-08-02 07:49:16 +0100

    updated the game pass accounts

--- programs/game_pass_new/src/states/alertstate.rs
+++ programs/game_pass_new/src/states/alertstate.rs
@@ -8,11 +8,3 @@ pub struct Alert {
     pub trigger_condition: String, // JSON string for customizable conditions
     pub game_id: Pubkey,
 }
-
-#[account]
-pub struct UserStreak {
-    pub user_game_account: Pubkey,
-    pub current_streak: u32,
-    pub longest_streak: u32,
-    pub last_played_at: i64,
-}
